c2b516e9d9fffa31528a67875a5bc29ead8df3f2
CANDELA KRUMMER MATEOS <43725125@comunidadort.edu.ar>
2021-11-04 15:44:04 -0300

    Proyecto en nuevo repositorio, pasado todo lo que habíamos hecho en clase

--- ProyectoClasesParticulares/Startup.cs
+++ ProyectoClasesParticulares/Startup.cs
@@ -8,6 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoClasesParticulares.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ProyectoClasesParticulares
 {
@@ -23,7 +28,16 @@ namespace ProyectoClasesParticulares
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for nonessential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ClasesParticularesDatabaseContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProyectoClasesParticularesDBConnection"]));
+
+            services.AddMvc().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
