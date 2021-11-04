76396420d6a41e7812c18afa6d77e6a4ae02f065
CANDELA KRUMMER MATEOS <43725125@comunidadort.edu.ar>
2021-11-04 15:19:43 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoClasesParticulares/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoClasesParticulares.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
